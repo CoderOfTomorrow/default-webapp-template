88c5148098fa715a73b6c5a72e6c5702748f0302
CoderOfTomorrow <nicolae.sb15@gmail.com>
2023-12-10 10:45:22 +0200

    feat(setup) : T-1 Initial project setup (#1)

---
+++ Default.WebApp.Template.AcceptanceTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Default.WebApp.Template.AcceptanceTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
