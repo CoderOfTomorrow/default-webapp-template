535f46c1986432949f2bbfdd98bf89075aed007c
CoderOfTomorrow <nicolae.sb15@gmail.com>
2023-12-11 07:42:14 +0200

    tests(unitTests) : T4 - Add packages for unit tests project (#4)

---
+++ Default.WebApp.Template.Tests/ApplicationDataAttribute.cs
@@ -0,0 +1,18 @@
+﻿using EntityFrameworkCore.AutoFixture.Core;
+using EntityFrameworkCore.AutoFixture.Sqlite;
+
+namespace Default.WebApp.Template.Tests
+{
+    public class ApplicationDataAttribute(params object[] arguments) : InlineAutoDataAttribute(() => new Fixture()
+                .Customize(new CompositeCustomization(
+                    new AutoNSubstituteCustomization(),
+                    new SqliteCustomization
+                    {
+                        AutoOpenConnection = true,
+                        OmitDbSets = true,
+                        OnCreate = OnCreateAction.EnsureCreated
+                    })),
+                arguments)
+    {
+    }
+}
