aced9c595d39787f8ee238ac062a1dbc0fddcb9f
CoderOfTomorrow <nicolae.sb15@gmail.com>
2023-12-22 09:05:04 +0200

    feat(auth) : T-5 Add basic login and register pages (#5)

--- Default.WebApp.Template.Client/Program.cs
+++ Default.WebApp.Template.Client/Program.cs
@@ -1,6 +1,7 @@
 using Default.WebApp.Template.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,5 +11,6 @@ builder.Services.AddHttpClient("Default.WebApp.Template.ServerAPI", client => cl
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Default.WebApp.Template.ServerAPI"));
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
